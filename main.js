0ff1cbe4ba4d26d840b9f1ddc4f69f9c497d73e8
Patyalvesena <patyalvesena@gmai.com>
2023-04-08 11:03:19 -0300

    Correção
    Ajustes de index e início de modificações  em JS

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const randomNumber = Math.round(Math.random() * 10)
 let xAttemps = 1
 
-function hideRun(event) {
+function change(event) {
     event.preventDefault()
 
     const inputNumber = document.querySelector('#inputNumber')
     
     if(Number(inputNumber.value) == randomNumber) {
         document.querySelector(".scren1").classList.add(".hide")
-        document.querySelector(".scren2").classList.remove(".hide")
+        document.querySelector(".scren2 hide").classList.remove(".hide")
 
         document.querySelector(".screen2 h2")
         .innerText = `Você acertou em ${xAttemps} tentativa` 
@@ -16,3 +16,8 @@ function hideRun(event) {
 
     xAttemps++
 }
+
+//Event
+
+const btnTry= document.querySelector("#btnTry")
+const btnReset= document.querySelector("#btnReset")
